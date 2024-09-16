27efa4d991a1ec5e3aafa6f402ebdfe58a886e86
Geovanny Mendoza <mgeovanny@uninorte.edu.co>
2024-09-16 12:34:02 -0500

    Update OnBoardingScreen
    Update OnBoardingScreen

--- app/src/main/java/com/geovannycode/jetpackcompose/ecoeats/presentation/on_boarding/OnBoardingScreen.kt
+++ app/src/main/java/com/geovannycode/jetpackcompose/ecoeats/presentation/on_boarding/OnBoardingScreen.kt
@@ -2,8 +2,14 @@ package com.geovannycode.jetpackcompose.ecoeats.presentation.on_boarding
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.geovannycode.jetpackcompose.ecoeats.presentation.previews.PreviewDefault
 
 @Composable
 fun OnBoardingScreen(modifier: Modifier = Modifier) {
+}
 
+@PreviewDefault
+@Composable
+fun OnBoardingScreenPreview(){
+    OnBoardingScreen()
 }
